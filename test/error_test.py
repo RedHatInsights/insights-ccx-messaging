01c0cf074de8bae6e76e7bb39eb46fd7a268ce06
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-11 15:10:47 +0100

    Test for error class

---
+++ test/error_test.py
@@ -0,0 +1,48 @@
+# Copyright 2020 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for error classes."""
+
+import pytest
+from collections import namedtuple
+
+from ccx_messaging.error import CCXMessagingError
+
+
+InputMessage = namedtuple("InputMessage", "topic partition offset value")
+
+input_msg = InputMessage(
+    topic="topic name",
+    partition="partition name",
+    offset=1234,
+    value={
+        "url": "any/url",
+        "identity": {"identity": {"internal": {"org_id": "12345678"}}},
+        "timestamp": "2020-01-23T16:15:59.478901889Z",
+        "ClusterName": "clusterName",
+    },
+)
+
+
+def test_error_formatting():
+    """Test the error formatting."""
+    err = CCXMessagingError("CCXMessagingError")
+    assert err is not None
+
+    fmt = err.format(input_msg)
+    expected = (
+        "Status: Error; Topic: topic name; Partition: partition name; "
+        + "Offset: 1234; Cluster: clusterName; Cause: CCXMessagingError"
+    )
+    assert fmt == expected
