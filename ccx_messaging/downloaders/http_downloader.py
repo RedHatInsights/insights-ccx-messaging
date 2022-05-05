43a1559df4fa9ba3c5a03c846755d4d59bfe0b56
Jan Dobes <jdobes@redhat.com>
2022-05-05 13:16:53 +0200

    Support minio URL in ephemeral environment

--- ccx_messaging/downloaders/http_downloader.py
+++ ccx_messaging/downloaders/http_downloader.py
@@ -70,7 +70,8 @@ class HTTPDownloader:
     HTTP_RE = re.compile(
         r"^(?:https://[^/]+\.s3\.amazonaws\.com/[0-9a-zA-Z/\-]+|"
         r"https://s3\.[0-9a-zA-Z\-]+\.amazonaws\.com/[0-9a-zA-Z\-]+/[0-9a-zA-Z/\-]+|"
-        r"http://minio:9000/insights-upload-perma/[0-9a-zA-Z\.\-]+/[0-9a-zA-Z\-]+)\?"
+        r"http://(env-ephemeral-[0-9a-zA-Z/\-]+-)?minio(\.ephemeral-[0-9a-zA-Z/\-]+\.svc)?:9000/"
+        r"insights-upload-perma/[0-9a-zA-Z\.\-]+/[0-9a-zA-Z\-]+)\?"
         r"X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=[^/]+$"
     )
 
