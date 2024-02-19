aaaadb987ee5ed852d9ee4aca04ea42d86d4af66
Jakub Drobena <jdrobena@redhat.com>
2024-02-19 13:16:54 +0100

    Making new consumer for internal data pipeline

--- ccx_messaging/consumers/internal_kafka_consumer.py
+++ ccx_messaging/consumers/internal_kafka_consumer.py
@@ -1,3 +1,4 @@
+"""Kafka consumer implementation using Confluent Kafka library for Internal Data Pipeline."""
 import logging
 from sentry_sdk import capture_exception
 from confluent_kafka import Message
@@ -5,6 +6,9 @@ from ccx_messaging.consumers.kafka_consumer import KafkaConsumer
 import json
 LOG = logging.getLogger(__name__)
 class Consumer(KafkaConsumer):
+
+    """Consumer based in Confluent Kafka for Internal Data Pipeline."""
+
     def __init__(
         self,
         publisher,
@@ -13,26 +17,30 @@ class Consumer(KafkaConsumer):
         **kwargs,
     ):
         """Initialise the KafkaConsumer object and related handlers."""
-        requeuer = kwargs.pop("requeuer", None)
+        kwargs.pop("requeuer", None)
         incoming_topic = kwargs.pop("incoming_topic")
         super().__init__(publisher, downloader, engine,incoming_topic,kwargs)
 
-    def get_url(self, input_msg: dict) -> str:  
+    def get_url(self, input_msg: dict) -> str:
+        """Retrieve URL to storage from Kafka message."""
         try:
             return input_msg.get("path")
-        except:
+        except Exception:
             return "input msg has no attribute get"
-        
+
     def handles(self, msg: Message) -> bool:
-        return True
-    
+        """Check if the message is usable."""
+        message = self.deserialize(msg.value())
+        if message.get("path") is not None:
+            return True
+        else:
+            return False
+
     def deserialize(self, msg):
-        """
-        Deserialize JSON message received from kafka
-        """
+        """Deserialize JSON message received from kafka."""
         try:
             return json.loads(msg.value())
         except (TypeError, json.JSONDecodeError) as ex:
-            LOG.error("Unable to deserialize JSON", extra=dict(ex=ex))
+            LOG.error("Unable to deserialize JSON", extra=dict(ex=ex))  # noqa: C408
             capture_exception(ex)
             return None
