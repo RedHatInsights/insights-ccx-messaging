e53a06435ac1abae393135353c997bfb6b4a6e41
José Luis Segura Lucas <joseluis@redhat.com>
2024-02-29 13:22:53 +0100

    Some pollishing and field testing for the s3_uploader

--- ccx_messaging/engines/s3_upload_engine.py
+++ ccx_messaging/engines/s3_upload_engine.py
@@ -2,13 +2,17 @@ import json
 import os
 import re
 import logging
-from insights_messaging.engine import Engine as ICMEngine
-from ccx_messaging.utils.idp_uploader import IDPUploader
+
+from insights_messaging.engine import Engine
+
+from ccx_messaging.utils.s3_uploader import S3Uploader
 from ccx_messaging.error import CCXMessagingError
+
 
 S3_ARCHIVE_PATTERN = re.compile(r"^([0-9]+)\/([0-9,a-z,-]{36})\/([0-9]{14})-[a-z,A-Z,0-9]*$")
 ARCHIVES_PATH_PREFIX = os.environ.get("SYNCED_ARCHIVES_PATH_PREFIX", "archives/compressed")
 LOG = logging.getLogger(__name__)
+
 
 def extract_org_id(file_path):
     search = re.search(S3_ARCHIVE_PATTERN,file_path)
@@ -20,6 +24,7 @@ def extract_org_id(file_path):
         )
         return
     return search.group(1)
+
 
 def compute_target_path(file_path):
     """
@@ -50,31 +55,41 @@ def compute_target_path(file_path):
     )
     return target_path
 
+
 def create_metadata(s3_path,cluster_id):
     org_id = extract_org_id(s3_path)
     msg_metadata = {"cluster_id": cluster_id, "external_organization":org_id}
     LOG.debug("msg_metadata %s", msg_metadata)
     return msg_metadata
 
-class IDPEngine(ICMEngine):
 
+class S3UploadEngine(Engine):
     def __init__(self, formatter,uploader, target_components=None, extract_timeout=None, extract_tmp_dir=None):  # noqa: E501
         self.dest_bucket = uploader.pop("bucket")
-        self.uploader = IDPUploader(**uploader)
+        self.uploader = S3Uploader(**uploader)
         super().__init__(formatter, target_components, extract_timeout, extract_tmp_dir)
-        
+
     def process(self, broker, local_path):
         self.dest_bucket = "PlaceholderText"
         cluster_id = broker["cluster_id"]
         s3_path = broker["s3_path"]
         del broker["cluster_id"]
         del broker["s3_path"]
+
         for w in self.watchers:
             w.watch_broker(broker)
+
         target_path = compute_target_path(s3_path)
         LOG.info(f"(NOT IMPLEMENTED) Uploading archive '{s3_path}' as {self.dest_bucket}/{target_path}")  # noqa: E501
+
         self.uploader.upload_file(local_path, self.dest_bucket, target_path)
         LOG.info(f"Uploaded archive '{s3_path}' as {self.dest_bucket}/{target_path}")
+
         metadata = create_metadata(s3_path,cluster_id)
-        kafka_msg = {"path":target_path,"original_path":s3_path,"metadata":metadata}
+        kafka_msg = {
+            "path": target_path,
+            "original_path": s3_path,
+            "metadata": metadata,
+        }
+
         return json.dumps(kafka_msg)
--- ccx_messaging/utils/s3_uploader.py
+++ ccx_messaging/utils/s3_uploader.py
@@ -1,15 +1,15 @@
 import logging
 from contextlib import contextmanager
-from insights_messaging.publishers import Publisher
+
 import boto3
 from botocore.exceptions import ClientError
+
 
 LOG = logging.getLogger(__name__)
 
 
-class IDPUploader(Publisher):
-
-    def __init__(self, **kwargs):  # noqa: D107
+class S3Uploader:
+    def __init__(self, **kwargs):
         access_key = kwargs.pop("access_key")
         secret_key = kwargs.get("secret_key", None)
         endpoint = kwargs.get("endpoint", None)
@@ -32,23 +32,9 @@ class IDPUploader(Publisher):
             endpoint_url=endpoint,
         )
 
-    @contextmanager
     def upload_file(self, path, bucket, file_name):
         LOG.info(f"Uploading '{file_name}' as '{path}' to '{bucket}'")
 
         with open(path, "rb") as file_data:
-            self.publish(bucket, file_data, file_name)
+            self.client.put_object(Bucket=bucket, Key=file_name, Body=file_data)
             LOG.info(f"Uploaded '{file_name}' as '{path}' to '{bucket}'")
-
-    def publish(self, bucket, file_data, file_name):
-        LOG.info(f"Uploading data to '{bucket}/{file_name}'")
-
-        try:
-            result = self.client.put_object(Bucket=bucket, Key=file_name, Body=file_data)
-            LOG.debug(f"Upload result from '{bucket}/{file_name}': {result}")
-            LOG.info(f"Uploaded data to '{bucket}/{file_name}'")
-        except ClientError as err:
-            LOG.error(
-                "ClientError while uploading data"+str(err),
-                extra={"bucket": bucket, "file_name": file_name, "err": err},
-            )
