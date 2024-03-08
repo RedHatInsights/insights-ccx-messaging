9a44306c40b887156c090082cba6814dd6db1505
Jakub Drobena <jdrobena@redhat.com>
2024-03-08 13:24:22 +0100

    Adding new UT's for s3_downloader

---
+++ test/downloaders/s3_downloader_test.py
@@ -0,0 +1,42 @@
+# Copyright 2024 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module containing unit tests for the `idp` class."""
+import os
+from moto import mock_aws
+import boto3
+from ccx_messaging.downloaders.s3_downloader import S3Downloader
+@mock_aws
+def test_download_existing_file():
+    """Test downloading of existing file in s3."""
+    file = open("testfile","a")
+    file.write("test")
+    file.close()
+    conn = boto3.resource('s3',aws_access_key_id="test",aws_secret_access_key="test")
+    bucket = conn.create_bucket(Bucket="testBucket")
+    bucket.upload_file("testfile","testfile")
+    downloader = S3Downloader(access_key="test",secret_key="test",bucket="testBucket")
+    with downloader.get("testfile") as path:
+      assert os.path.exists(path)
+    os.remove("testfile")
+
+@mock_aws
+def test_download_non_existing_file():
+    """Test downloading of non existing file in s3."""
+    conn = boto3.resource('s3',aws_access_key_id="test",aws_secret_access_key="test")
+    conn.create_bucket(Bucket="testBucket")
+    downloader = S3Downloader(access_key="test",secret_key="test",bucket="testBucket")
+    try:
+        downloader.get("non_existend_file")
+    except Exception as ex:
+        assert isinstance(ex,FileNotFoundError)
