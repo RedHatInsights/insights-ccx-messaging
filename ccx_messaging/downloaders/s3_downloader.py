b99aa127267258026ed57da6eb96b3a2a89e8806
Jakub Drobena <jdrobena@redhat.com>
2024-03-08 14:32:02 +0100

    Adding UT's

--- ccx_messaging/downloaders/s3_downloader.py
+++ ccx_messaging/downloaders/s3_downloader.py
@@ -1,8 +1,27 @@
-from contextlib import contextmanager
+# Copyright 2024 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""S3 Downloader for internal data pipeline."""
+
 from insights_messaging.downloaders.s3 import S3Downloader as S3Downloader
 
 class IDPDownloader(S3Downloader):
+
+    """Downloader for S3 bucket."""
+
     def __init__(self, **kwargs):
+        """Set up the S3 downloader."""
         if not kwargs['access_key']:
             raise ConfigurationError("Access Key environment variable not set.")
         if not kwargs['secret_key']:
@@ -18,8 +37,12 @@ class IDPDownloader(S3Downloader):
         super().__init__(key=self.access_key,secret=self.secret_key,client_kwargs={'endpoint_url':self.endpoint_url})
 
     def get(self, path):
+        """Download the archive from given path."""
         return super().get(f"{self.bucket}/{path}")
 
 
 class ConfigurationError(Exception):
+
+    """Configuration error."""
+
     pass
