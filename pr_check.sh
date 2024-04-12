9239dcf0afc327abebc15abdc62790533681afe2
Juan Díaz <juandiazsuarez0@gmail.com>
2024-04-12 14:53:56 +0200

    fix typo

--- pr_check.sh
+++ pr_check.sh
@@ -4,7 +4,7 @@ CICD_TOOLS_URL="https://raw.githubusercontent.com/RedHatInsights/cicd-tools/main
 # shellcheck source=/dev/null
 source <(curl -sSL "$CICD_TOOLS_URL") image_builder
 
-export CICD_IMAGE_BUILDER_IMAGE_NAME='quay.io/cloudservices/insights-ccx-messaging'
+export CICD_IMAGE_BUILDER_IMAGE_NAME='quay.io/cloudservices/ccx-messaging'
 export CICD_IMAGE_BUILDER_ADDITIONAL_TAGS=("latest")
 
 cicd::image_builder::build_and_push
