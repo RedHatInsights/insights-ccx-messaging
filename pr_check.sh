629c010fb09897cf05487ee04e5b1e3f25ed5a46
Victor M. <vmugicag@redhat.com>
2024-04-23 10:59:53 +0200

    fix(ci): handle pr_check errors

--- pr_check.sh
+++ pr_check.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 CICD_TOOLS_URL="https://raw.githubusercontent.com/RedHatInsights/cicd-tools/main/src/bootstrap.sh"
-# shellcheck source=/dev/null
-source <(curl -sSL "$CICD_TOOLS_URL") image_builder
-
 export CICD_IMAGE_BUILDER_IMAGE_NAME='quay.io/cloudservices/ccx-messaging'
 export CICD_IMAGE_BUILDER_ADDITIONAL_TAGS=("latest")
 
-cicd::image_builder::build_and_push
+# shellcheck source=/dev/null
+if ! source <(curl -sSL "$CICD_TOOLS_URL") image_builder; then
+    echo "Error loading image_builder module!"
+    exit 1
+fi
+
+if ! cicd::image_builder::build_and_push; then
+    echo "Error building image!"
+    exit 1
+fi
 
 # Temporary stub
 mkdir -p artifacts
