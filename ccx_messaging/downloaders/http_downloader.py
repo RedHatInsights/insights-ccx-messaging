c3c3d0635bedf45fdd7a616fde1b8134898bd774
Juan Diaz Suarez <jdiazsua@redhat.com>
2024-04-24 08:44:40 +0200

    log error message when downloading archive

--- ccx_messaging/downloaders/http_downloader.py
+++ ccx_messaging/downloaders/http_downloader.py
@@ -116,6 +116,8 @@ class HTTPDownloader:
             response.close()
 
         except requests.exceptions.ConnectionError as err:
+            LOG.error("Connection error while downloading the file: %s", err)
             raise CCXMessagingError("Connection error while downloading the file") from err
         except Exception as err:
+            LOG.error("Unknown error while downloading the file: %s", err)
             raise CCXMessagingError("Unknown error while downloading the file") from err
