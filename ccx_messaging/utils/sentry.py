c5c12da0eb32b959d6c55a7fe5d2899c364aa960
Jakub007d <56787294+Jakub007d@users.noreply.github.com>
2025-03-27 13:37:13 +0100

    Update sentry.py

--- ccx_messaging/utils/sentry.py
+++ ccx_messaging/utils/sentry.py
@@ -28,17 +28,18 @@ def get_event_level():
     return logging.ERROR
 
 
-def init_sentry(dsn=None, transport=None, environment=None):
+def init_sentry(dsn=None, transport=None, environment=None, enabled=False):
     """Configure and initialize sentry SDK for this project."""
-    if dsn:
-        logging.getLogger(__name__).info("Initializing sentry")
-        sentry_logging = LoggingIntegration(level=logging.INFO, event_level=get_event_level())
-
-        sentry_sdk.init(
-            dsn=dsn,
-            ca_certs="/etc/pki/tls/certs/ca-bundle.crt",
-            integrations=[sentry_logging],
-            max_breadcrumbs=15,
-            transport=transport,
-            environment=environment,
-        )
+    if enabled:
+        if dsn:
+            logging.getLogger(__name__).info("Initializing sentry")
+            sentry_logging = LoggingIntegration(level=logging.INFO, event_level=get_event_level())
+    
+            sentry_sdk.init(
+                dsn=dsn,
+                ca_certs="/etc/pki/tls/certs/ca-bundle.crt",
+                integrations=[sentry_logging],
+                max_breadcrumbs=15,
+                transport=transport,
+                environment=environment,
+            )
