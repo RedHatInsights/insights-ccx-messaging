0d9ad031ffe2a06d2df091f8b0237b979b876cff
Iker Reyes <ireyes@redhat.com>
2025-05-06 15:44:09 +0200

    Use image tag env var

--- pr_check.sh
+++ pr_check.sh
@@ -34,9 +34,9 @@ EXTRA_DEPLOY_ARGS="\
     --set-parameter archive-sync-ols/IMAGE=quay.io/cloudservices/ccx-messaging \
     --set-parameter rules-uploader/IMAGE=quay.io/cloudservices/ccx-messaging
 "
-IQE_IMAGE="quay.io/cloudservices/iqe-tests:ccx"
 
 export IQE_PLUGINS="ccx"
+export IQE_IMAGE_TAG="ccx"
 # Run all pipeline and ui tests
 export IQE_MARKER_EXPRESSION="internal"
 export IQE_FILTER_EXPRESSION=""
