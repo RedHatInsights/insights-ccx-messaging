b73846b2c9a246ffc44a58710a711c7b94d6e773
Jakub007d <dr.kubko@gmail.com>
2025-08-12 15:30:28 +0200

    Memray update

--- ccx_messaging/command_line.py
+++ ccx_messaging/command_line.py
@@ -11,6 +11,7 @@ from insights_messaging.appbuilder import AppBuilder
 
 from ccx_messaging.utils.clowder import apply_clowder_config
 from ccx_messaging.utils.logging import setup_watchtower
+
 from ccx_messaging.utils.sentry import init_sentry
 
 
@@ -19,6 +20,16 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("config", nargs="?", help="Application configuration.")
     parser.add_argument("--version", help="Show version.", action="store_true")
+    parser.add_argument(
+        "--memray-profile",
+        help="Enable memray memory profiling and specify output file path",
+        metavar="OUTPUT_FILE",
+    )
+    parser.add_argument(
+        "--memray-native-mode",
+        help="Enable native mode for memray profiling (tracks C/C++ allocations)",
+        action="store_true",
+    )
     return parser.parse_args()
 
 
@@ -44,7 +55,8 @@ def print_version() -> None:
         pass
 
 
-def apply_config(config) -> int:
+
+def apply_config(config, memray_output=None, native_mode=False) -> int:
     """Apply configuration file provided as argument and run consumer."""
     with open(config) as file_:
         if isClowderEnabled() and os.getenv("CLOWDER_ENABLED") in ["True", "true", "1", "yes"]:
@@ -56,15 +68,37 @@ def apply_config(config) -> int:
         logging_config = app_builder.service["logging"]
         logging.config.dictConfig(logging_config)
         print_version()
-        try:
-            consumer = app_builder.build_app()
-            setup_watchtower(logging_config)
-            consumer.run()
-            return 0
+        
+        if memray_output:
+            logger = logging.getLogger(__name__)
+            logger.info("Starting memray profiling, output file: %s", memray_output)
+            logger.info("Native mode: %s", native_mode)
+            
+            try:
+                import memray
+            except ImportError:
+                logger.error("memray not available. Install with: pip install memray")
+                return 1
+            
+            with memray.Tracker(memray_output, native_traces=native_mode):
+                return _run_consumer(app_builder, logging_config)
+        else:
+            return _run_consumer(app_builder, logging_config)
 
-        except ModuleNotFoundError as ex:
-            logging.error("Module not found: %s. Did you miss some dependency?", ex.name)
-            return 1
+
+def _run_consumer(app_builder, logging_config) -> int:
+    """Run the consumer application."""
+    try:
+        consumer = app_builder.build_app()
+        setup_watchtower(logging_config)
+        
+        consumer.run()
+        return 0
+
+    except ModuleNotFoundError as ex:
+        logging.error("Module not found: %s. Did you miss some dependency?", ex.name)
+        return 1
+
 
 
 def ccx_messaging() -> None:
@@ -84,7 +118,11 @@ def ccx_messaging() -> None:
     )
 
     if args.config:
-        retval = apply_config(args.config)
+        # Allow environment variables to override CLI args for memray
+        memray_output = args.memray_profile or os.environ.get("MEMRAY_PROFILE")
+        native_mode = args.memray_native_mode or os.environ.get("MEMRAY_NATIVE_MODE", "false").lower() == "true"
+        
+        retval = apply_config(args.config, memray_output, native_mode)
         sys.exit(retval)
 
     logger = logging.getLogger(__name__)
